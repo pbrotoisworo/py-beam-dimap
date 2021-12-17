508cc8983799a1d0cc47fdd9f425c70b54b976fe
Panji Brotoisworo <pbrotoisworo@stamina4space.upd.edu.ph>
2021-12-17 22:56:23 +0800

    Update README. Add setup.py

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # py-beam-dimap-reader
 A Python interface to easily parse and interact with the BEAM-DIMAP XML file which is used as part of SNAP software for Sentinel satellites.
+
+The metadata parameters are parsed and structured as a Python dictionary for easier navigation. 
+# Features
+* Extract processing history including operator used and the parameters used
+* Extract metadata text
+
+# Usage
+
+```py
+dim_file = 'Sentinel1_SLC.dim'
+dimap = BeamDimap(dim_file)
+
+# Get 11th item in the processing history
+dimap.get_processing_history(11)
+# {'node': 'node.11', 'operator': 'Multilook', 'parameters': {'nAzLooks': '2', 'nRgLooks': '6', 'outputIntensity': 'false', 'grSquarePixel': 'true'}}
+
+# Get mission
+dimap.get_abstracted_metadata('MISSION')
+# {'@name': 'MISSION', '@desc': 'Satellite mission', '@unit': '', '@type': 'ascii', '@mode': 'rw', '#text': 'SENTINEL-1B'}
+
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pybeamdimap',
+    author='Panji P. Brotoisworo',
+    url='https://github.com/pbrotoisworo/py-beam-dimap',
+    version=0.1,
+    description='Interface to easily parse and navigate BEAM-DIMAP XML data',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'xmltodict'
+    ],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="Copernicus, Dimap, S2nap, Remote Sensing",
+)
