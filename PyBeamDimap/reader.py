ad254143720a846006b9ec6b0eae544f63d4af4e
Panji Brotoisworo <pbrotoisworo@stamina4space.upd.edu.ph>
2021-12-20 02:53:30 +0800

    Add tests for Sentinel-1 BEAM-DIMAP
    - S1 insar multiband tests
    - S1 insar single band tests
    - New _DictToClassObject class

--- PyBeamDimap/reader.py
+++ PyBeamDimap/reader.py
@@ -22,15 +22,15 @@ class BeamDimap:
         # Load important metadata
         self.metadata_format = self.metadata['Dimap_Document']['Metadata_Id']['METADATA_FORMAT']['#text']
         self.metadata_version = self.metadata['Dimap_Document']['Metadata_Id']['METADATA_FORMAT']['@version']
-        self.product_description = self.get_abstracted_metadata_attribute('SPH_DESCRIPTOR')
-        self.mission = self.get_abstracted_metadata_attribute('MISSION')
+        self.product_description = self.get_abstracted_metadata_attribute('SPH_DESCRIPTOR').text
+        self.mission = self.get_abstracted_metadata_attribute('MISSION').text
         self.dataset_name = self.metadata['Dimap_Document']['Dataset_Id']['DATASET_NAME']
         self.crs = self.metadata['Dimap_Document']['Coordinate_Reference_System']['WKT']
 
     def get_abstracted_metadata_attribute(self, name: str):
         for i, elem in enumerate(self._abstracted_metadata):
             if elem['@name'] == name:
-                return dict(self._abstracted_metadata[i])
+                return _DictToClassObject(dict(self._abstracted_metadata[i]), 'AbstractedMetadata')
         return
 
     def get_band_info(self, band_index=None):
@@ -42,15 +42,16 @@ class BeamDimap:
             return self._band_info
         else:
             if band_index > band_count:
-                raise IndexError(f'User inputted band index "{band_index}" is greater than maximum index "{band_count}"')
+                raise IndexError(
+                    f'User inputted band index "{band_index}" is greater than maximum index "{band_count}"')
             return self._band_info[band_index]
+            # return _DictKeyToClassObject(self._band_info[band_index])
 
     def get_processing_history(self, node_index=None):
-
         if node_index is None:
             return self._processing_history
         else:
-            return self._processing_history[node_index]
+            return _DictToClassObject(self._processing_history[node_index], section='processing')
 
     def _load_image_interpretation(self):
         """
@@ -60,25 +61,37 @@ class BeamDimap:
         band_metadata = []
 
         for elem in bands:
+
+            # Get target elem. The element structure changes slightly depending if multiband or singleband
+            if isinstance(elem, str):
+                # Single band
+                target_elem = bands
+                band_path = self.metadata['Dimap_Document']['Data_Access']['Data_File']['DATA_FILE_PATH']['@href']
+
+            else:
+                # Multiband
+                target_elem = elem
+                band_index = target_elem.get('BAND_INDEX')
+                band_path = self.metadata['Dimap_Document']['Data_Access']['Data_File'][int(band_index)][
+                    'DATA_FILE_PATH']['@href']
+
             band_metadata.append({
-                'band_index': elem.get('BAND_INDEX'),
-                'band_path':
-                    self.metadata['Dimap_Document']['Data_Access']['Data_File'][int(elem.get('BAND_INDEX'))][
-                        'DATA_FILE_PATH']['@href'],
-                'band_description': elem.get('BAND_DESCRIPTION'),
-                'band_name': elem.get('BAND_NAME'),
-                'band_raster_width': elem.get('BAND_RASTER_WIDTH'),
-                'band_raster_height': elem.get('BAND_RASTER_HEIGHT'),
-                'data_type': elem.get('DATA_TYPE'),
-                'band_wavelen': elem.get('BAND_WAVELEN'),
-                'bandwidth': elem.get('BANDWIDTH'),
-                'scaling_factor': elem.get('SCALING_FACTOR'),
-                'scaling_offset': elem.get('SCALING_OFFSET'),
-                'log10_scaled': elem.get('LOG10_SCALED'),
-                'no_data_value_used': elem.get('NO_DATA_VALUE_USED'),
-                'no_data_value': elem.get('NO_DATA_VALUE'),
-                'valid_mask_term': elem.get('VALID_MASK_TERM'),
-                'image_to_model_transform': elem.get('IMAGE_TO_MODEL_TRANSFORM')
+                'BAND_INDEX': target_elem.get('BAND_INDEX'),
+                'BAND_PATH': band_path,
+                'BAND_DESCRIPTION': target_elem.get('BAND_DESCRIPTION'),
+                'BAND_NAME': target_elem.get('BAND_NAME'),
+                'BAND_RASTER_WIDTH': target_elem.get('BAND_RASTER_WIDTH'),
+                'BAND_RASTER_HEIGHT': target_elem.get('BAND_RASTER_HEIGHT'),
+                'DATA_TYPE': target_elem.get('DATA_TYPE'),
+                'BAND_WAVELEN': target_elem.get('BAND_WAVELEN'),
+                'BANDWIDTH': target_elem.get('BANDWIDTH'),
+                'SCALING_FACTOR': target_elem.get('SCALING_FACTOR'),
+                'SCALING_OFFSET': target_elem.get('SCALING_OFFSET'),
+                'LOG10_SCALED': target_elem.get('LOG10_SCALED'),
+                'NO_DATA_VALUE_USED': target_elem.get('NO_DATA_VALUE_USED'),
+                'NO_DATA_VALUE': target_elem.get('NO_DATA_VALUE'),
+                'VALID_MASK_TERM': target_elem.get('VALID_MASK_TERM'),
+                'IMAGE_TO_MODEL_TRANSFORM': target_elem.get('IMAGE_TO_MODEL_TRANSFORM')
             })
         return band_metadata
 
@@ -115,13 +128,52 @@ class BeamDimap:
             except (KeyError, TypeError):
                 parameters = None
 
-            history.append(
-                {'node': elem['@name'],
-                 'operator': elem['MDATTR'][1]['#text'],
-                 'parameters': parameters}
-            )
+            # Get processing time
+            try:
+                processing_time = elem['MDATTR'][8]['#text']
+            except IndexError:
+                processing_time = None
 
+            # Get sources
+            sources = {}
+            # print(elem['@name'], elem['MDATTR'][1]['#text'])
+            target_source_elem = dict(elem['MDElem'][0].items())
+            if target_source_elem.get('MDATTR') is None:
+                sources = None
+            else:
+                target_source_elem = target_source_elem['MDATTR']#['#text']
+                if isinstance(target_source_elem, list):
+                    for item in target_source_elem:
+                        sources[item['@name']] = item['#text']
+                else:
+                    sources[target_source_elem['@name']] = target_source_elem['#text']
+
+            history_dict = {
+                'node': elem['@name'],
+                 'operator': elem['MDATTR'][1]['#text'],
+                 'purpose': elem['MDATTR'][4]['#text'],
+                 'processingTime': processing_time,
+                 'sources': sources,
+                 'parameters': parameters
+            }
+
+            history.append(history_dict)
         return history
+
+
+class _DictToClassObject:
+
+    def __init__(self, node: dict, section: str):
+        """
+        Dynamically create class based on dictionary object
+        :param node: Dictionary containing node data
+        """
+        for key, value in node.items():
+            if section == 'processing':
+                pass
+            if section == 'AbstractedMetadata':
+                key = key[1:]
+            setattr(_DictToClassObject, key, value)
 
 
 if __name__ == '__main__':
---
+++ tests/test_s1_insar_multiband.py
@@ -0,0 +1,144 @@
+import json
+import os
+import subprocess
+import sys
+
+import pytest
+
+from PyBeamDimap.reader import BeamDimap
+
+TEST_DIR = os.path.abspath('tests')
+data2 = os.path.join(TEST_DIR, 'S1_DInSARStack_20190902_20190914.dim')
+
+
+def assert_error(expected, actual):
+    return f'Error with extracted XML value. Expecting {expected}. Got {actual}.'
+
+
+#######################################
+# Test multiband Sentinel-1 SLC data
+#######################################
+
+@pytest.fixture
+def dimap():
+    """
+    Load an instance of BEAM-DIMAP reader with single band SLC data
+    """
+    # Setup testing environment
+    yield BeamDimap(metadata=data2)
+
+
+def test_data2_important_metadata(dimap):
+    """
+    Test important metadata parameters
+    """
+    # Check extracted data is correct
+    expected = 'DIMAP'
+    assert dimap.metadata_format == expected, assert_error(expected, dimap.metadata_format)
+
+    expected = '2.12.1'
+    assert dimap.metadata_version == expected, assert_error(expected, dimap.metadata_version)
+
+    expected = 'Sentinel-1 IW Level-1 SLC Product'
+    assert dimap.product_description == expected, assert_error(expected, dimap.product_description)
+
+    expected = 'SENTINEL-1B'
+    assert dimap.mission == expected, assert_error(expected, dimap.mission)
+
+    expected = '20190902_20190914_DInSARStack'
+    assert dimap.dataset_name == expected, assert_error(expected, dimap.dataset_name)
+
+
+def test_data2_band_info(dimap):
+
+    actual = dimap.get_band_info(0)['BAND_RASTER_WIDTH']
+    expected = '5282'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(1)['BAND_RASTER_WIDTH']
+    expected = '5282'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(5)['BAND_DESCRIPTION']
+    expected = None
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(0)['BAND_NAME']
+    expected = 'Intensity_ifg_VV_02Sep2019_14Sep2019'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(1)['BAND_NAME']
+    expected = 'Intensity_ifg_VV_02Sep2019_14Sep2019_db'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(2)['BAND_NAME']
+    expected = 'Phase_ifg_VV_02Sep2019_14Sep2019'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(3)['BAND_NAME']
+    expected = 'coh_IW2_VV_02Sep2019_14Sep2019'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(4)['BAND_NAME']
+    expected = 'Unw_Phase_ifg_02Sep2019_14Sep2019_VV'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_band_info(5)['BAND_NAME']
+    expected = 'displacement_VV_slv1_02Sep2019'
+    assert actual == expected, assert_error(expected, actual)
+
+
+def test_data2_abstracted_metadata(dimap):
+
+    actual = dimap.get_abstracted_metadata_attribute('Processing_system_identifier').text
+    expected = 'ESA Sentinel-1 IPF 003.10'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_abstracted_metadata_attribute('PRODUCT').text
+    expected = 'S1B_IW_SLC__1SDV_20190902T075741_20190902T075808_017856_0219A5_70FA'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_abstracted_metadata_attribute('PASS').text
+    expected = 'DESCENDING'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_abstracted_metadata_attribute('centre_lat').text
+    expected = '64.27210588794522'
+    assert actual == expected, assert_error(expected, actual)
+
+
+def test_data1_processing_graph(dimap):
+
+    actual = dimap.get_processing_history(0).operator
+    expected = 'Read'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(0).parameters['copyMetadata']
+    expected = 'true'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(4).parameters['resamplingType']
+    expected = 'BILINEAR_INTERPOLATION'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(5).operator
+    expected = 'Enhanced-Spectral-Diversity'
+    assert actual == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(4).sources
+    expected = 'file:/E:/SAR_Iceland/sample/20190902_Orb.dim'
+    assert actual['sourceProduct'] == expected, assert_error(expected, actual)
+    expected = 'file:/E:/SAR_Iceland/sample/20190914_Orb.dim'
+    assert actual['sourceProduct.1'] == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(1).sources
+    expected = 'file:/E:/SAR_Iceland/images/S1B_IW_SLC__1SDV_20190902T075741_20190902T075808_017856_0219A5_70FA.zip'
+    assert actual['sourceProduct'] == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(17).sources
+    expected = 'file:/E:/SAR_Iceland/sample/20190902_20190914_Orb_Stack_Ifg_Deb_TopoPhase_ML_Flt_Unw_TC.dim'
+    assert actual['sourceProduct.0'] == expected, assert_error(expected, actual)
+
+    actual = dimap.get_processing_history(17).sources
+    expected = 'file:/E:/SAR_Iceland/sample/20190902_20190914_disp_TC.dim'
+    assert actual['sourceProduct.1'] == expected, assert_error(expected, actual)
